a4e335dcc6f2a13e6da75639a06fdfa0257f70cb
Stelios Trimis <stltrimis@gmail.com>
2022-08-23 20:42:08 +0300

    Email Administration

---
+++ EmailAdministration/readme.md
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<h2>Email Administration</h2>
+
+<h4><u>This app is a simple project who shows you, how to make an API who can create bussiness emails for a new Worker.<br>
+</u></h4>
+
+</body>
+</html>
---
+++ EmailAdministration/src/emailadministration/EmailAdministration.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package emailadministration;
+
+/**
+ *
+ * @author straq
+ */
+public class EmailAdministration {
+
+    
+    public static void main(String[] args) {
+        Email em1 = new Email("John","Smith");
+        
+        System.out.println(em1.showInfo());
+    }
+    
+}
